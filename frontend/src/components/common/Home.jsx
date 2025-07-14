5840e232e58fb9bd7198dbaa26b5037ffea620e9
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-07-14 22:32:35 +0700

    Creating a Common Layout

--- frontend/src/components/common/Home.jsx
+++ frontend/src/components/common/Home.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import "swiper/css";
 import LatestProducts from "./LatestProducts";
 import FeatureProducts from "./FeatureProducts";
-import Header from "./Header";
-import Footer from "./Footer";
 import Hero from "./Hero";
+import Layout from "./Layout";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Hero />
-      <LatestProducts />
-      <FeatureProducts />
-      <Footer />
+      <Layout>
+        <Hero />
+        <LatestProducts />
+        <FeatureProducts />
+      </Layout>
     </>
   );
 };
