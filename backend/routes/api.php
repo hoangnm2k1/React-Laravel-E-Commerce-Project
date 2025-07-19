dae116f4ef809d062ffdf92e00e2d3f529edac70
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-07-19 16:24:21 +0700

    Optimized Routes with Resource

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,9 +12,12 @@ Route::post('/admin/login', [AuthController::class, 'authenticate']);
 // })->middleware('auth:sanctum');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('categories/{id}', [CategoryController::class, 'show']);
-    Route::post('categories', [CategoryController::class, 'store']);
-    Route::put('categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+    // Route::get('categories', [CategoryController::class, 'index']);
+    // Route::get('categories/{id}', [CategoryController::class, 'show']);
+    // Route::post('categories', [CategoryController::class, 'store']);
+    // Route::put('categories/{id}', [CategoryController::class, 'update']);
+    // Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('brands', CategoryController::class);
 });
