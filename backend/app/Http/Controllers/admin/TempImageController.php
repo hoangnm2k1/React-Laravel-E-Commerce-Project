3b5b65988430f7262ba59d269e6ff2e200b99b34
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-07-22 10:49:55 +0700

    Save Temp Image

---
+++ backend/app/Http/Controllers/admin/TempImageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\TempImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class TempImageController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|mimes:jpeg,png,jpg,gif',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'error' => $validator->errors()
+                ], 422);
+        }
+
+        $image = $request->file('image');
+        $imageName = time() . '.' . $image->extension();
+        $image->move(public_path('uploads/temp'), $imageName);
+
+        $tempImage = new TempImage();
+        $tempImage->name = $imageName;
+        $tempImage->save();
+
+        $manager = new ImageManager(Driver::class);
+        $img = $manager->read(public_path('uploads/temp/' . $imageName));
+        $img->resize(400, 450);
+        $img->save(public_path('uploads/temp/thumbnail/' . $imageName));
+
+        return response()->json([
+            'status' => 200,
+            'data' => $tempImage
+        ]);
+
+    }
+}
